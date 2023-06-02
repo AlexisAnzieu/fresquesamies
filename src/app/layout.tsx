f4bc69381ab5b3390a27854df156418c55ff24b8
alexis anzieu <alexis.anzieu@ssense.com>
2023-06-02 14:23:13 -0400

    push analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <Providers> {children}</Providers>
+                <Analytics />
             </body>
         </html>
     );
